4d1cfc2d5853135f81e5e155c80a9e76f0250faf
Neil Cudden <ncudden@sbcglobal.net>
2017-09-01 12:41:13 -0700

    Initial version of resume to use as starting point

--- JS/resume1a.js
+++ JS/resume1a.js
@@ -1,70 +1,50 @@
-
-
-/*
-$('#goto1').css({
-    'background-image': 'url(../IMAGES/F5E.jpg)'
-});
-
-$scrollButtons.css({
-    'position': 'relative',
-    'margin': '-5px auto 10px auto',
-    'width': '400px',
-    'height': '50px',
-    'background-color': '#3c3c41',
-    'border': '3px solid #7b7b81',
-    'border-radius': '25px'
-});
-
-$button1.css({
-    'position': 'relative',
-    'margin-top': '17px',
-    'margin-left': '34px',
-    'margin-right': '34px',
-    'width': '10px',
-    'height': '10px',
-    'background-color': '#7b7b81',
-    'border': '2px solid #7b7b81',
-    'border-radius': '5px',
-    'float': 'left'
-});
-
-*/
-
 /* -----  Functions  ----- */
 
-
-
-/*var moveLft =function(){
-    setButtonCol();
-    if($('.picture').attr('id') == 'goto5'){
-        $goto.removeAttr('id');
-        $goto.attr('id','goto4');
-        $('#goto4').css({
-            'background-image': 'url(../IMAGES/F18A.jpg)'
-        });
-        $('#button4').css({
-            'background-color': '#7b7b81'
-        });
-    
+var nextFrame = function(){
+    if ($('.shown').attr('id') == 'frame1'){
+        $('#frame1').removeClass('shown').addClass('notShown');
+        $('#frame2').removeClass('notShown').addClass('shown');
+    } else if ($('.shown').attr('id') == 'frame2'){
+        $('#frame2').removeClass('shown').addClass('notShown');
+        $('#frame3').removeClass('notShown').addClass('shown');
+    } else if ($('.shown').attr('id') == 'frame3'){
+        $('#frame3').removeClass('shown').addClass('notShown');
+        $('#frame4').removeClass('notShown').addClass('shown');
+    } else if ($('.shown').attr('id') == 'frame4'){
+        $('#frame4').removeClass('shown').addClass('notShown');
+        $('#frame5').removeClass('notShown').addClass('shown');
+    } else if ($('.shown').attr('id') == 'frame5'){
+        $('#frame5').removeClass('shown').addClass('notShown');
+        $('#frame1').removeClass('notShown').addClass('shown');
     } else {
-        $goto.removeAttr('id');
-        $goto.attr('id','goto5');
-        $('#goto5').css({
-            'background-image': 'url(../IMAGES/V22.jpg)'
-        });
-        $('#button5').css({
-            'background-color': '#7b7b81'
-        });
+        console.log('You forgot to add the new frames!!');
     }
-   
-    $('.picture').hide("fade",1);
-    $('.picture').show("fade",1000);
 };
 
-var setButtonCol =function(){
-    $('.button').css({
-            'background-color': '#fff'
-        });
+var prevFrame = function(){
+    if ($('.shown').attr('id') == 'frame1'){
+        $('#frame1').removeClass('shown').addClass('notShown');
+        $('#frame5').removeClass('notShown').addClass('shown');
+    } else if ($('.shown').attr('id') == 'frame5'){
+        $('#frame5').removeClass('shown').addClass('notShown');
+        $('#frame4').removeClass('notShown').addClass('shown');
+    } else if ($('.shown').attr('id') == 'frame4'){
+        $('#frame4').removeClass('shown').addClass('notShown');
+        $('#frame3').removeClass('notShown').addClass('shown');
+    } else if ($('.shown').attr('id') == 'frame3'){
+        $('#frame3').removeClass('shown').addClass('notShown');
+        $('#frame2').removeClass('notShown').addClass('shown');
+    } else if ($('.shown').attr('id') == 'frame2'){
+        $('#frame2').removeClass('shown').addClass('notShown');
+        $('#frame1').removeClass('notShown').addClass('shown');
+    } else {
+        console.log('You forgot to add the new frames!!');
+    }  
+};
+
+/*   
+    $('.picture').hide("fade",1);
+    $('.picture').show("fade",1000);
 };*/
 
 
@@ -90,7 +70,15 @@ $(function(){
     });
     
     $('.myIframe').css('height', $('#box1').height()+'px');
-    /*$('.myIframe').css('height', $(window).height()+'px');*/
+    
+    $('#lftArrow').on('click',function(){
+        prevFrame();
+    });
+    
+    $('#rtArrow').on('click',function(){
+        var $rtClick = $('.myIframe');
+        nextFrame($rtClick);
+    });
 
     /*
     $('#button1').on('click',function(){
