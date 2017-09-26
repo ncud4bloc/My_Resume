12008c34f98efbbfeb4a16448da61deb1f836964
Neil Cudden <ncudden@sbcglobal.net>
2017-09-26 10:37:32 -0700

    updates to resume

--- JS/ncc_ResR.js
+++ JS/ncc_ResR.js
@@ -3,60 +3,100 @@
 var nextFrame = function(){
     $('.notShown').hide("fade",1);
     if ($('.shown').attr('id') == 'frame1'){
+        $('#box1').css({
+            'background-image': 'url(IMAGES/Myframe_snip2.jpg)'    
+        });
         $('#frame1').removeClass('shown').addClass('notShown');
         $('#frame2').removeClass('notShown').addClass('shown');
     } else if ($('.shown').attr('id') == 'frame2'){
+        $('#box1').css({
+            'background-image': 'url(IMAGES/Diner_snip2.jpg)'    
+        });
         $('#frame2').removeClass('shown').addClass('notShown');
         $('#frame3').removeClass('notShown').addClass('shown');
     } else if ($('.shown').attr('id') == 'frame3'){
+        $('#box1').css({
+            'background-image': 'url(IMAGES/Newsweek_snip.jpg)'    
+        });
         $('#frame3').removeClass('shown').addClass('notShown');
         $('#frame4').removeClass('notShown').addClass('shown');
     } else if ($('.shown').attr('id') == 'frame4'){
+        $('#box1').css({
+            'background-image': 'url(IMAGES/Calculator_snip.jpg)'    
+        });
         $('#frame4').removeClass('shown').addClass('notShown');
         $('#frame5').removeClass('notShown').addClass('shown');
     } else if ($('.shown').attr('id') == 'frame5'){
+        $('#box1').css({
+            'background-image': 'url(IMAGES/Ttt_snip2.jpg)'    
+        });
         $('#frame5').removeClass('shown').addClass('notShown');
         $('#frame6').removeClass('notShown').addClass('shown');
     } else if ($('.shown').attr('id') == 'frame6'){
+        $('#box1').css({
+            'background-image': 'url(IMAGES/Snake_snip.jpg)'    
+        });
         $('#frame6').removeClass('shown').addClass('notShown');
         $('#frame7').removeClass('notShown').addClass('shown');
     } else if ($('.shown').attr('id') == 'frame7'){
+        $('#box1').css({
+            'background-image': 'url(IMAGES/Minesweeper_snip2.jpg)'    
+        });
         $('#frame7').removeClass('shown').addClass('notShown');
         $('#frame1').removeClass('notShown').addClass('shown');
     } else {
         console.log('You forgot to add the new frames!!');
     }
-    /*$('.shown').show("fade",500);*/
     $('.shown').slideDown(500);
 };
 
 var prevFrame = function(){
     $('.notShown').hide("fade",1);
     if ($('.shown').attr('id') == 'frame1'){
+        $('#box1').css({
+            'background-image': 'url(IMAGES/Myframe_snip2.jpg)'    
+        });
         $('#frame1').removeClass('shown').addClass('notShown');
         $('#frame7').removeClass('notShown').addClass('shown');
     } else if ($('.shown').attr('id') == 'frame7'){
+        $('#box1').css({
+            'background-image': 'url(IMAGES/Minesweeper_snip2.jpg)'    
+        });
         $('#frame7').removeClass('shown').addClass('notShown');
         $('#frame6').removeClass('notShown').addClass('shown');
     } else if ($('.shown').attr('id') == 'frame6'){
+        $('#box1').css({
+            'background-image': 'url(IMAGES/Snake_snip.jpg)'    
+        });
         $('#frame6').removeClass('shown').addClass('notShown');
         $('#frame5').removeClass('notShown').addClass('shown');
     } else if ($('.shown').attr('id') == 'frame5'){
+        $('#box1').css({
+            'background-image': 'url(IMAGES/Ttt_snip2.jpg)'    
+        });
         $('#frame5').removeClass('shown').addClass('notShown');
         $('#frame4').removeClass('notShown').addClass('shown');
     } else if ($('.shown').attr('id') == 'frame4'){
+        $('#box1').css({
+            'background-image': 'url(IMAGES/Calculator_snip.jpg)'    
+        });
         $('#frame4').removeClass('shown').addClass('notShown');
         $('#frame3').removeClass('notShown').addClass('shown');
     } else if ($('.shown').attr('id') == 'frame3'){
+        $('#box1').css({
+            'background-image': 'url(IMAGES/Newsweek_snip.jpg)'    
+        });
         $('#frame3').removeClass('shown').addClass('notShown');
         $('#frame2').removeClass('notShown').addClass('shown');
     } else if ($('.shown').attr('id') == 'frame2'){
+        $('#box1').css({
+            'background-image': 'url(IMAGES/Diner_snip2.jpg)'    
+        });
         $('#frame2').removeClass('shown').addClass('notShown');
         $('#frame1').removeClass('notShown').addClass('shown');
     } else {
         console.log('You forgot to add the new frames!!');
     }
-    /*$('.shown').show("fade",500);*/
     $('.shown').slideDown(500);
 };
 
@@ -80,8 +120,7 @@ $(function(){
     });
     
     $('#rtArrow').on('click',function(){
-        var $rtClick = $('.myIframe');
-        nextFrame($rtClick);
+        nextFrame();
     });
 
 });
