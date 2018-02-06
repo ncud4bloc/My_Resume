04b9c74ceaa1478c84d0ce078226f9be3a65b521
Neil Cudden <ncudden@sbcglobal.net>
2018-02-06 09:58:59 -0700

    Iteration 2 Resume update 1 on 2-6-2018

--- Iteration2/JS/resumeScripts.js
+++ Iteration2/JS/resumeScripts.js
@@ -1,5 +1,18 @@
+/* -----  Functions  ----- */
+
+function showInfo(){
+    console.log("Run showInfo function");
+}
+
+function unShowInfo(){
+    console.log("Run unShowInfo function");
+}
+
+
+/* -----  Function Calls  ----- */
+
 $(function(){
     
-    
+    $('#box13').hover(showInfo,unShowInfo);
 
 });
