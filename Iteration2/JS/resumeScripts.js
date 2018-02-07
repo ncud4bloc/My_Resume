aa23a980323eda1d523c112046b20727e6c4d9e9
Neil Cudden <ncudden@sbcglobal.net>
2018-02-06 17:30:30 -0700

    Iteration 2 Resume update 3 on 2-6-2018

--- Iteration2/JS/resumeScripts.js
+++ Iteration2/JS/resumeScripts.js
@@ -1,3 +1,30 @@
+/* -----  Variables  ----- */
+
+
+var pageWidth = ($(window).width());
+    console.log('Window width is ' + pageWidth);
+
+var $box14 = $('#box14');
+var $box15 = $('#box15');
+var $box16 = $('#box16');
+var $box17 = $('#box17');
+var $box18 = $('#box18');
+var $box19 = $('#box19');
+
+//var $b14Title = $('<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 dataPage" id="b14Title"><p>Ruby Hangman:</p></div>');
+
+var $b15Title = $('<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 dataPage" id="b15Title"><p>P51 Motorcycle:</p></div>');
+var $b15Link1 = $('<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 dataPage" id="b15Link1"><p><a href="https://github.com/ncud4bloc/My_Frame/" target="_blank">Source Code</a></p></div>');
+var $b15Link2 = $('<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 dataPage" id="b15Link2"><p><a href="https://ncud4bloc.github.io/My_Frame/HTML/index.html" target="_blank">View in Browser</a></p></div>');
+var $b15Desc = $('<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dataPage" id="b15Desc"><p>Responsive motorcycle manufacturer homepage using my own grid-based framework. I thought that this company\'s homepage was a good concept poorly executed, and this is my "fix" (the company has since updated its page).</p></div>');
+
+
+/*var $b16Title = $('<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 dataPage" id="b16Title"><p>Mesa Diner:</p></div>');
+var $b17Title = $('<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 dataPage" id="b17Title"><p>Air Images:</p></div>');
+var $b18Title = $('<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 dataPage" id="b18Title"><p>Google Maps:</p></div>');
+var $b19Title = $('<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 dataPage" id="b19Title"><p>Missile Command:</p></div>');*/
+
+
 /* -----  Functions  ----- */
 
 
@@ -15,9 +42,24 @@ var showInfo = function(){
     } else if ($('.active').attr('id') == 'box14'){
         console.log("box14 active");
         $('#box14').removeClass('active').addClass('inActive');
+        
+        
     } else if ($('.active').attr('id') == 'box15'){
         console.log("box15 active");
+        $box16.append($b15Title);
+        $box16.append($b15Link1);
+        $box16.append($b15Link2);
+        $box16.append($b15Desc);
+        
+        /*$tabs.css({
+            'position': 'fixed',
+            'width': '100%',
+            'height': '50px',
+            'left': '220px',
+            'top': '225px'
+        });*/
         $('#box15').removeClass('active').addClass('inActive');
+        
     } else if ($('.active').attr('id') == 'box16'){
         console.log("box16 active");
         $('#box16').removeClass('active').addClass('inActive');
@@ -71,8 +113,6 @@ var unShowInfo = function(){
 
 $(function(){
     
-    //$('.projBox').hover(showInfo,unShowInfo);
-    
     $('#box4').hover(showInfo,unShowInfo);
     $('#box5').hover(showInfo,unShowInfo);
     $('#box6').hover(showInfo,unShowInfo);
@@ -84,5 +124,4 @@ $(function(){
     $('#box18').hover(showInfo,unShowInfo);
     $('#box19').hover(showInfo,unShowInfo);
     
-
 });
