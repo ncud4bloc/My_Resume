421b10789307aa9d429959836bf5a5048dac52c5
Neil Cudden <ncudden@sbcglobal.net>
2018-02-09 14:30:12 -0700

    Iteration 2 Resume update 2 on 2-9-2018

--- Iteration2/JS/resumeScripts.js
+++ Iteration2/JS/resumeScripts.js
@@ -3,18 +3,15 @@
 
 var pageWidth = ($(window).width());
     console.log('Window width is ' + pageWidth);
-
 var $box14 = $('#box14');
 var $box15 = $('#box15');
 var $box16 = $('#box16');
 var $box17 = $('#box17');
 var $box18 = $('#box18');
 var $box19 = $('#box19');
-
 var $box14t16 = $('#box14t16');
+var $box17t19 = $('#box17t19');
 
-
-//var $b14Title = $('<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 dataPage" id="b14Title"><p>Ruby Hangman:</p></div>');
 var $b14Slide = $('<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dataPage" id="b14Slide"></div>')
 var $b14Title = $('<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 dataPage" id="b14Title"><p>Ruby Hangman:</p></div>');
 var $b14Link1 = $('<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 dataPage" id="b14Link1"><p><a href="https://github.com/ncud4bloc/Hangman/" target="_blank">Source Code</a></p></div>');
@@ -26,11 +23,29 @@ var $b15Link1 = $('<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 dataPage" id=
 var $b15Link2 = $('<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 dataPage" id="b15Link2"><p><a href="https://ncud4bloc.github.io/My_Frame/HTML/index.html" target="_blank">View in Browser</a></p></div>');
 var $b15Desc = $('<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dataPage" id="b15Desc"><p>Responsive motorcycle manufacturer homepage using my own grid-based framework. I thought that this company\'s homepage was a good concept poorly executed, and this is my "fix" (the company has since updated its page).</p></div>');
 
+var $b16Slide = $('<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dataPage" id="b16Slide"></div>')
+var $b16Title = $('<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 dataPage" id="b16Title"><p>Mesa Diner:</p></div>');
+var $b16Link1 = $('<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 dataPage" id="b16Link1"><p><a href="https://github.com/ncud4bloc/Diner/" target="_blank">Source Code</a></p></div>');
+var $b16Link2 = $('<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 dataPage" id="b16Link2"><p><a href="https://ncud4bloc.github.io/Diner/HTML/index.html" target="_blank">View in Browser</a></p></div>');
+var $b16Desc = $('<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dataPage" id="b16Desc"><p>DOM manipulation using JavaScript & jQuery.</p></div>');
 
-/*var $b16Title = $('<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 dataPage" id="b16Title"><p>Mesa Diner:</p></div>');
-var $b17Title = $('<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 dataPage" id="b17Title"><p>Air Images:</p></div>');
-var $b18Title = $('<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 dataPage" id="b18Title"><p>Google Maps:</p></div>');
-var $b19Title = $('<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 dataPage" id="b19Title"><p>Missile Command:</p></div>');*/
+var $b17Slide = $('<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dataPage" id="b17Slide"></div>')
+var $b17Title = $('<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 dataPage" id="b17Title"><p>Image Slider:</p></div>');
+var $b17Link1 = $('<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 dataPage" id="b17Link1"><p><a href="https://github.com/ncud4bloc/ImageSlider/" target="_blank">Source Code</a></p></div>');
+var $b17Link2 = $('<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 dataPage" id="b17Link2"><p><a href="https://ncud4bloc.github.io/ImageSlider/HTML/index_STD.html" target="_blank">View in Browser</a></p></div>');
+var $b17Desc = $('<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dataPage" id="b17Desc"><p>Image slider using jQuery DOM manipulation and effects.</p></div>');
+
+var $b18Slide = $('<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dataPage" id="b18Slide"></div>')
+var $b18Title = $('<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 dataPage" id="b18Title"><p>Google Map:</p></div>');
+var $b18Link1 = $('<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 dataPage" id="b18Link1"><p><a href="https://github.com/ncud4bloc/GoogleMaps/" target="_blank">Source Code</a></p></div>');
+var $b18Link2 = $('<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 dataPage" id="b18Link2"><p><a href="https://ncud4bloc.github.io/GoogleMaps/HTML/indexGM.html" target="_blank">View in Browser</a></p></div>');
+var $b18Desc = $('<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dataPage" id="b18Desc"><p>A simple responsive website incorporating the Google Maps API. Enter a location in decimal degrees along with a marker message (how about 40.6892, -74.0445, Lady Liberty for starters?)</p></div>');
+
+var $b19Slide = $('<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dataPage" id="b19Slide"></div>')
+var $b19Title = $('<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 dataPage" id="b19Title"><p>Missile Command:</p></div>');
+var $b19Link1 = $('<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 dataPage" id="b19Link1"><p><a href="https://github.com/ncud4bloc/MissileCommand/" target="_blank">Source Code</a></p></div>');
+var $b19Link2 = $('<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 dataPage" id="b19Link2"><p><a href="https://ncud4bloc.github.io/MissileCommand/HTML/indexMC.html" target="_blank">View in Browser</a></p></div>');
+var $b19Desc = $('<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dataPage" id="b19Desc"><p>Web-based Missile Command classic arcade game generated using jQuery and Canvas.</p></div>');
 
 
 /* -----  Functions  ----- */
@@ -57,7 +72,6 @@ var showInfo = function(){
         $b14Slide.slideUp(0);
         $b14Slide.slideDown(800);
         
-        
     } else if ($('.active').attr('id') == 'box15'){
         console.log("box15 active");
         $box14t16.append($b15Slide);
@@ -65,26 +79,63 @@ var showInfo = function(){
             $b15Slide.append($b15Link1);
             $b15Slide.append($b15Link2);
             $b15Slide.append($b15Desc);
-        /*
-        $('.dataPage').css({
-            'display': 'block'
-        });
-        */
         $b15Slide.css({
             'display': 'block'
         });
-        
         $b15Slide.slideUp(0);
         $b15Slide.slideDown(800);
         
     } else if ($('.active').attr('id') == 'box16'){
         console.log("box16 active");
+        $box14t16.append($b16Slide);
+            $b16Slide.append($b16Title);
+            $b16Slide.append($b16Link1);
+            $b16Slide.append($b16Link2);
+            $b16Slide.append($b16Desc);
+        $b16Slide.css({
+            'display': 'block'
+        });
+        $b16Slide.slideUp(0);
+        $b16Slide.slideDown(800);
+        
     } else if ($('.active').attr('id') == 'box17'){
         console.log("box17 active");
+        $box17t19.append($b17Slide);
+            $b17Slide.append($b17Title);
+            $b17Slide.append($b17Link1);
+            $b17Slide.append($b17Link2);
+            $b17Slide.append($b17Desc);
+        $b17Slide.css({
+            'display': 'block'
+        });
+        $b17Slide.slideUp(0);
+        $b17Slide.slideDown(800);
+        
     } else if ($('.active').attr('id') == 'box18'){
         console.log("box18 active");
+        $box17t19.append($b18Slide);
+            $b18Slide.append($b18Title);
+            $b18Slide.append($b18Link1);
+            $b18Slide.append($b18Link2);
+            $b18Slide.append($b18Desc);
+        $b18Slide.css({
+            'display': 'block'
+        });
+        $b18Slide.slideUp(0);
+        $b18Slide.slideDown(800);
+        
     } else if ($('.active').attr('id') == 'box19'){
         console.log("box19 active");
+        $box17t19.append($b19Slide);
+            $b19Slide.append($b19Title);
+            $b19Slide.append($b19Link1);
+            $b19Slide.append($b19Link2);
+            $b19Slide.append($b19Desc);
+        $b19Slide.css({
+            'display': 'block'
+        });
+        $b19Slide.slideUp(0);
+        $b19Slide.slideDown(800);
     }
 };
 
@@ -99,35 +150,44 @@ var unShowInfo = function(){
     } else if ($('.active').attr('id') == 'box14'){
         console.log("box14 inactive");
         $b14Slide.slideUp(0);
-        
         $b14Slide.css({
             'display': 'none'
         });
         
-        
     } else if ($('.active').attr('id') == 'box15'){
         console.log("box15 inactive");
-        
         $b15Slide.slideUp(0);
-        
-        /*
-        $('.dataPage').css({
-            'display': 'none'
-        });
-        */
         $b15Slide.css({
             'display': 'none'
         });
-        
-        
+         
     } else if ($('.active').attr('id') == 'box16'){
         console.log("box16 inactive");
+        $b16Slide.slideUp(0);
+        $b16Slide.css({
+            'display': 'none'
+        });
+        
     } else if ($('.active').attr('id') == 'box17'){
         console.log("box17 inactive");
+        $b17Slide.slideUp(0);
+        $b17Slide.css({
+            'display': 'none'
+        });
+        
     } else if ($('.active').attr('id') == 'box18'){
         console.log("box18 inactive");
+        $b18Slide.slideUp(0);
+        $b18Slide.css({
+            'display': 'none'
+        });
+        
     } else if ($('.active').attr('id') == 'box19'){
         console.log("box19 inactive");
+        $b19Slide.slideUp(0);
+        $b19Slide.css({
+            'display': 'none'
+        });
     }
     $(this).removeClass('active').addClass('inActive'); //NCC
 };
